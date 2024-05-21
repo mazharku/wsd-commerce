8464f1bb681339fae931bb3630d5e065d737f267
mazhar <mazhar1322@cseku.ac.bd>
2024-05-21 19:42:04 +0600

    add meta data for login sales and products

--- src/main/resources/db/migration/V4__role_meta_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO wsd.roles (id, role_name, is_active) VALUES (1,'ADMIN',true);
-INSERT INTO wsd.roles (id, role_name, is_active) VALUES (2,'CASHIER',true);
-INSERT INTO wsd.roles (id, role_name, is_active) VALUES (3,'CUSTOMER',true);
